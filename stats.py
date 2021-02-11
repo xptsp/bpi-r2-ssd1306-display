08af06a611d906394c90b748302e8eb9338e789c
Douglas Orend <43975081+xptsp@users.noreply.github.com>
2021-02-11 10:06:08 -0600

    Update stats.py

--- stats.py
+++ stats.py
@@ -100,8 +100,7 @@ def get_ip_address(ifname):
 # Our signal handler to clear the screen upon exiting the script:
 ###################################################################################
 def signal_handler(sig, frame):
-	draw.rectangle((0,0,width,height), outline=0, fill=0)
-	disp.image(image)
+	disp.clear()
 	disp.display()
 	sys.exit(0)
 
@@ -109,72 +108,99 @@ signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGINT, signal_handler)
 
 ###################################################################################
-# Our loop, showing information:
+# Main loop which shows information about our computer:
 ###################################################################################
 while True:
+	###############################################################################
 	# Show the globe symbol if the WAN interface up and cable connected.
+	###############################################################################
 	try:
 		with open("/sys/class/net/" + wan_interface + "/operstate") as f:
-			state = f.read().strip()
-		if state != "up":
-			throw
+			if f.read().strip() != "up":
+				throw
 		image.paste(globe, (32 * 0, 0))
 		draw.text((0, 30), "WAN:  " + get_ip_address("wan"),  font=font, fill=255)
 	except:
 		draw.text((0, 30), "WAN:  Down",  font=font, fill=255)
 
+	###############################################################################
 	# Show the globe symbol if the 2.4Ghz wifi interface is up.
+	###############################################################################
 	draw.text((32 * 1, 16), "2.4G",  font=font, fill=255)
 	try:
 		with open("/sys/class/net/" + w24_interface + "/operstate") as f:
-			state = f.read().strip()
-		if state == "up":
-			throw
-		image.paste(wifi, (32 * 1, 0))
+			if f.read().strip() != "up":
+				throw
+		image.paste(wifi, (32 * 1 + 4, 0))
 	except:
 		image.paste(no_wifi, (32 * 1 + 4, 0))
 
+	###############################################################################
 	# Show the globe symbol if the 5Ghz wifi interface is up.
+	###############################################################################
 	draw.text((32 * 2, 16), "5GHz",  font=font, fill=255)
 	try:
 		with open("/sys/class/net/" + w5G_interface + "/operstate") as f:
-			state = f.read().strip()
-		if state != "up":
-			throw
+			if f.read().strip() != "up":
+				throw
 		image.paste(wifi, (32 * 2 + 4, 0))
 	except:
 		image.paste(no_wifi, (32 * 2 + 4, 0))
 
-	# Show the VPN symbol if the VPN_IN interface up and cable connected.
+	###############################################################################
+	# Show the VPN symbol if the VPN_IN interface is up.
+	###############################################################################
 	try:
 		with open("/sys/class/net/" + vpn_interface + "/operstate") as f:
-			state = f.read().strip()
-		if state == "up":
-			throw
+			if f.read().strip() != "up":
+				throw
 		image.paste(vpn, (32 * 3, 0))
 	except:
-		image.paste(vpn, (32 * 3, 0))
+		pass
 
-	# Write the CPU load values:
+	###############################################################################
+	# Write the CPU load values
+	###############################################################################
 	load = os.getloadavg()
-	txt = '{a:.2f}'
-	draw.text((0, 38), "Load: " + txt.format(a = load[0]) + "," + txt.format(a = load[1]) + "," + txt.format(a = load[2]),  font=font, fill=255)
+	draw.text((0, 38), "Load: " + '{:.2f}'.format(load[0]) + "," + '{:.2f}'.format(load[1]) + "," + '{:.2f}'.format(load[2]),  font=font, fill=255)
 
-	# Write the available and total memory:
+	###############################################################################
+	# Write the available and total memory
+	###############################################################################
 	mem = psutil.virtual_memory()
 	total = int(mem.total / 1024 / 1024)
 	used = total - int(mem.available / 1024 / 1024)
 	percent = int(used / total * 100)
-	draw.text((0, 46), "Mem:  " + "{:4d}".format(used) + "M/" + "{:4d}".format(total) + "M " + "{:2d}".format(percent) + "%", font=font, fill=255)
+	if used < 10000:
+		s1 = "{:4d}M".format(used)
+	else:
+		s1 = "{:2.1f}G".format(used / 1024)
+	if total < 10000:
+		s2 = "{:4d}M".format(total)
+	else:
+		s2 = "{:2.1f}G".format(total / 1024)
+	draw.text((0, 46), "Mem:  " + s1 + "/" + s2 + " " + "{:2d}".format(percent) + "%", font=font, fill=255)
 
-	# Write the available and total disk space:
+	###############################################################################
+	# Write the available and total disk space
+	###############################################################################
 	mem = psutil.disk_usage('/')
 	total = int(mem.total / 1024 / 1024)
 	used = total - int(mem.free / 1024 / 1024)
 	percent = int(used / total * 100)
-	draw.text((0, 54), "Disk: " + "{:4d}".format(used) + "M/" + "{:4d}".format(total) + "M " + "{:2d}".format(percent) + "%", font=font, fill=255)
+	if used < 10000:
+		s1 = "{:4d}M".format(used)
+	else:
+		s1 = "{:2.1f}G".format(used / 1024)
+	if total < 10000:
+		s2 = "{:4d}M".format(total)
+	else:
+		s2 = "{:2.1f}G".format(total / 1024)
+	draw.text((0, 54), "Disk: " + s1 + "/" + s2 + " " + "{:2d}".format(percent) + "%", font=font, fill=255)
 	
+	###############################################################################
 	# Display image.
+	###############################################################################
 	disp.image(image)
 	disp.display()
 	time.sleep(.1)
