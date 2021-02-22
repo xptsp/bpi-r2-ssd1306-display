c2a07e76930f1acce9be0cdb4419ff67de0bbd30
Frank Wunderlich <frank-w@public-files.de>
2021-02-22 18:59:45 +0100

    stats: rework to allow testing without display/i2c

--- stats.py
+++ stats.py
@@ -21,6 +21,13 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 ###################################################################################
+
+use_img=False
+use_img=True
+
+use_disp=False
+use_disp=True
+
 import os
 import time
 import socket
@@ -29,12 +36,15 @@ import struct
 import psutil
 import signal
 import sys
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
+#import Adafruit_GPIO.SPI as SPI
 
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
+if use_disp:
+    import Adafruit_SSD1306
+
+if use_img:
+    from PIL import Image
+    from PIL import ImageDraw
+    from PIL import ImageFont
 
 ###################################################################################
 # Change these variables to reflect which network adapter to use during this check
@@ -44,65 +54,67 @@ w24_interface = "wlp1s0"
 w5G_interface = "wlp1s0"
 vpn_interface = "vpn_in"
 
-###################################################################################
-# Setup for script execution:
-###################################################################################
-# Raspberry Pi pin configuration:
-RST = None     # on the PiOLED this pin isnt used
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
+i2cbus=2
+disp_w=128
+disp_h=64
 
-# Initialize library.
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_bus=2)
-disp.begin()
+if use_disp:
+    # Initialize library.
+    disp = Adafruit_SSD1306.SSD1306_128_64(rst=None, i2c_bus=i2cbus)
+    disp.begin()
 
-# Clear display.
-disp.clear()
-disp.display()
+    # Clear display.
+    disp.clear()
+    disp.display()
 
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
+if use_img:
+    if use_disp:
+        width = disp.width
+        height = disp.height
+    else:
+        width=disp_w
+        height=disp_h
 
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
+    image = Image.new('1', (width, height))
 
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
+    # Get drawing object to draw on image.
+    draw = ImageDraw.Draw(image)
 
-# Load default font.
-font = ImageFont.load_default()
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-###################################################################################
-# Load images once into memory:
-###################################################################################
-globe = Image.open('images/globe.png').convert('1')
-wifi  = Image.open('images/wifi.png').convert('1')
-no_wifi = Image.open('images/no-wifi.png').convert('1')
-vpn   = Image.open('images/vpn.png').convert('1')
+    # Load default font.
+    font = ImageFont.load_default()
+
+    ###################################################################################
+    # Load images once into memory:
+    ###################################################################################
+    globe = Image.open('images/globe.png').convert('1')
+    wifi  = Image.open('images/wifi.png').convert('1')
+    no_wifi = Image.open('images/no-wifi.png').convert('1')
+    vpn   = Image.open('images/vpn.png').convert('1')
 
 ###################################################################################
 # Function to get IP address about a specific network adapter:
 ###################################################################################
 def get_ip_address(ifname):
-	s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	return socket.inet_ntoa(fcntl.ioctl(
-		s.fileno(),
-		0x8915,  # SIOCGIFADDR
-		struct.pack('256s', ifname[:15].encode('UTF-8'))
-	)[20:24])
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15].encode('UTF-8'))
+    )[20:24])
 
 ###################################################################################
 # Our signal handler to clear the screen upon exiting the script:
 ###################################################################################
 def signal_handler(sig, frame):
-	disp.clear()
-	disp.display()
-	sys.exit(0)
+    if use_disp:
+        disp.clear()
+        disp.display()
+    sys.exit(0)
 
 signal.signal(signal.SIGTERM, signal_handler)
 signal.signal(signal.SIGINT, signal_handler)
@@ -111,99 +123,135 @@ signal.signal(signal.SIGINT, signal_handler)
 # Main loop which shows information about our computer:
 ###################################################################################
 while True:
-	# Draw a black filled box to clear the image.
-	draw.rectangle((0,0,width,height), outline=0, fill=0)
-	
-	###############################################################################
-	# Show the globe symbol if the WAN interface up and cable connected.
-	###############################################################################
-	try:
-		with open("/sys/class/net/" + wan_interface + "/operstate") as f:
-			if f.read().strip() != "up":
-				throw
-		image.paste(globe, (32 * 0, 0))
-		draw.text((0, 30), "WAN:  " + get_ip_address(wan_interface),  font=font, fill=255)
-	except:
-		draw.text((0, 30), "Cable Disconnected",  font=font, fill=255)
+    ###############################################################################
+    # Show the globe symbol if the WAN interface up and cable connected.
+    ###############################################################################
+    wan_txt="WAN: "
+    wan_ico=None
+    try:
+        with open("/sys/class/net/" + wan_interface + "/operstate") as f:
+            if f.read().strip() != "up":
+                throw
+        wan_ico=globe
+        wan_txt+= get_ip_address(wan_interface)
+    except:
+        wan_txt+="disconnected"
+    print(wan_txt)
 
-	###############################################################################
-	# Show the globe symbol if the 2.4Ghz wifi interface is up.
-	###############################################################################
-	draw.text((32 * 1, 16), "2.4G",  font=font, fill=255)
-	try:
-		with open("/sys/class/net/" + w24_interface + "/operstate") as f:
-			if f.read().strip() != "up":
-				throw
-		image.paste(wifi, (32 * 1 + 4, 0))
-	except:
-		image.paste(no_wifi, (32 * 1 + 4, 0))
 
-	###############################################################################
-	# Show the globe symbol if the 5Ghz wifi interface is up.
-	###############################################################################
-	draw.text((32 * 2, 16), "5GHz",  font=font, fill=255)
-	try:
-		with open("/sys/class/net/" + w5G_interface + "/operstate") as f:
-			if f.read().strip() != "up":
-				throw
-		image.paste(wifi, (32 * 2 + 4, 0))
-	except:
-		image.paste(no_wifi, (32 * 2 + 4, 0))
+    ###############################################################################
+    # Show the globe symbol if the 2.4Ghz wifi interface is up.
+    ###############################################################################
+    wifi24_txt="2G4: "
+    wifi24_ico=None
+    try:
+        with open("/sys/class/net/" + w24_interface + "/operstate") as f:
+            if f.read().strip() != "up":
+                throw
+        wifi24_ico=wifi
+        wifi24_txt+="up"
+    except:
+        wifi24_ico=no_wifi
+        wifi24_txt+="none"
+    print(wifi24_txt)
 
-	###############################################################################
-	# Show the VPN symbol if the VPN_IN interface is up.
-	###############################################################################
-	try:
-		with open("/sys/class/net/" + vpn_interface + "/operstate") as f:
-			if f.read().strip() != "up":
-				throw
-		image.paste(vpn, (32 * 3, 0))
-	except:
-		pass
+    ###############################################################################
+    # Show the globe symbol if the 5Ghz wifi interface is up.
+    ###############################################################################
+    wifi5_txt="5G: "
+    wifi5_ico=None
+    try:
+        with open("/sys/class/net/" + w5G_interface + "/operstate") as f:
+            if f.read().strip() != "up":
+                throw
+        wifi5_ico=wifi
+        wifi5_txt+="up"
+    except:
+        wifino_ico=no_wifi
+        wifi5_txt+="none"
+    print(wifi5_txt)
 
-	###############################################################################
-	# Write the CPU load values
-	###############################################################################
-	load = os.getloadavg()
-	draw.text((0, 38), "Load: " + '{:.2f}'.format(load[0]) + "," + '{:.2f}'.format(load[1]) + "," + '{:.2f}'.format(load[2]),  font=font, fill=255)
+    ###############################################################################
+    # Show the VPN symbol if the VPN_IN interface is up.
+    ###############################################################################
+    vpn_ico=None
+    try:
+        with open("/sys/class/net/" + vpn_interface + "/operstate") as f:
+            if f.read().strip() != "up":
+                throw
+        vpn_ico=vpn
+    except:
+        pass
 
-	###############################################################################
-	# Write the available and total memory
-	###############################################################################
-	mem = psutil.virtual_memory()
-	total = int(mem.total / 1024 / 1024)
-	used = total - int(mem.available / 1024 / 1024)
-	percent = int(used / total * 100)
-	if used < 10000:
-		s1 = "{:4d}M".format(used)
-	else:
-		s1 = "{:2.1f}G".format(used / 1024)
-	if total < 10000:
-		s2 = "{:4d}M".format(total)
-	else:
-		s2 = "{:2.1f}G".format(total / 1024)
-	draw.text((0, 46), "Mem:  " + s1 + "/" + s2 + " " + "{:2d}".format(percent) + "%", font=font, fill=255)
+    ###############################################################################
+    # Write the CPU load values
+    ###############################################################################
+    load = os.getloadavg()
+    load_txt="Load: " + '{:.2f}'.format(load[0]) + "," + '{:.2f}'.format(load[1]) + "," + '{:.2f}'.format(load[2])
+    print(load_txt)
 
-	###############################################################################
-	# Write the available and total disk space
-	###############################################################################
-	mem = psutil.disk_usage('/')
-	total = int(mem.total / 1024 / 1024)
-	used = total - int(mem.free / 1024 / 1024)
-	percent = int(used / total * 100)
-	if used < 10000:
-		s1 = "{:4d}M".format(used)
-	else:
-		s1 = "{:2.1f}G".format(used / 1024)
-	if total < 10000:
-		s2 = "{:4d}M".format(total)
-	else:
-		s2 = "{:2.1f}G".format(total / 1024)
-	draw.text((0, 54), "Disk: " + s1 + "/" + s2 + " " + "{:2d}".format(percent) + "%", font=font, fill=255)
-	
-	###############################################################################
-	# Display image.
-	###############################################################################
-	disp.image(image)
-	disp.display()
-	time.sleep(.1)
+    ###############################################################################
+    # Write the available and total memory
+    ###############################################################################
+    mem = psutil.virtual_memory()
+    total = int(mem.total / 1024 / 1024)
+    used = total - int(mem.available / 1024 / 1024)
+    percent = int(used / total * 100)
+    if used < 10000:
+        s1 = "{:4d}M".format(used)
+    else:
+        s1 = "{:2.1f}G".format(used / 1024)
+    if total < 10000:
+        s2 = "{:4d}M".format(total)
+    else:
+        s2 = "{:2.1f}G".format(total / 1024)
+    mem_txt="Mem:  " + s1 + "/" + s2 + " " + "{:2d}".format(percent) + "%"
+    print(mem_txt)
+
+    ###############################################################################
+    # Write the available and total disk space
+    ###############################################################################
+    mem = psutil.disk_usage('/')
+    total = int(mem.total / 1024 / 1024)
+    used = total - int(mem.free / 1024 / 1024)
+    percent = int(used / total * 100)
+    if used < 10000:
+        s1 = "{:4d}M".format(used)
+    else:
+        s1 = "{:2.1f}G".format(used / 1024)
+    if total < 10000:
+        s2 = "{:4d}M".format(total)
+    else:
+        s2 = "{:2.1f}G".format(total / 1024)
+    disk_txt="Disk: " + s1 + "/" + s2 + " " + "{:2d}".format(percent) + "%"
+    print(disk_txt)
+
+    ###############################################################################
+    # Generate image.
+    ###############################################################################
+    if use_img:
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0,0,width,height), outline=0, fill=0)
+        if wan_ico:
+           image.paste(wan_ico, (32 * 0, 0))
+        if wifi24_ico:
+            image.paste(wifi24_ico, (32 * 1 + 4, 0))
+        draw.text((32 * 1, 16), "2.4G",  font=font, fill=255)
+        if wifi5_ico:
+            image.paste(wifi5_ico, (32 * 2 + 4, 0))
+        draw.text((32 * 2, 16), "5GHz",  font=font, fill=255)
+        if vpn_ico:
+            image.paste(vpn_ico, (32 * 3, 0))
+        draw.text((0, 30), wan_txt,  font=font, fill=255)
+        draw.text((0, 38), load_txt,  font=font, fill=255)
+        draw.text((0, 46), mem_txt, font=font, fill=255)
+        draw.text((0, 54), disk_txt, font=font, fill=255)
+        if use_disp:
+            ###############################################################################
+            # Display image.
+            ###############################################################################
+            disp.image(image)
+            disp.display()
+        else:
+            image.save('/home/frank/stats.png')
+    time.sleep(5)
